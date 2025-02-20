27fbf5f441c5f0c171a3942692a9d5f47362a0d0
ldrever <lawrence.drever@gmail.com>
2025-02-20 13:33:35 +0000

    Progress towards working from the RIGHT

--- Problem0206.java
+++ Problem0206.java
@@ -1,5 +1,7 @@
 package euler;
 
+import java.util.*;
+
 public class Problem0206 {
 	/*
 	 * 
@@ -45,6 +47,17 @@ public class Problem0206 {
 			}
 		}
 		
+		HashSet<Long> input = new HashSet<>();
+		input.add(1L);
+		HashSet<Long> output = valuesModTenPower(1, 6, input);
+		
+		System.out.println(output.toString());
+		
+		HashSet<Long> output2 = valuesModTenPower(2, 16, output);
+		
+		System.out.println(output2.toString());
+		
+		
 		
 	}
 	
@@ -60,5 +73,51 @@ public class Problem0206 {
 		return output;
 	}
 	
+	public static void printEveryExample(String input) {
+		
+		// great now let's recurse
+		
+		// input will be something like 1620344_5_6
+		
+		// it'll run itself 10 times, with the leftmost blank switched for
+		// some digit
+		
+		// we're going to need to do better than this... billions of base-cases implied...
+	}
 	
+	/*
+	 * How about this. We can look at the final digit of the target, then
+	 * place limits MOD 10 on what the answer can be.
+	 * 
+	 * Then look at the last TWO digits, and place limits MOD 100, having
+	 * ALREADY filtered these based on the MOD 10 requirement. And so on!
+	 */
+	
+	// FIXME bigIntegers of course!
+	public static HashSet<Long> valuesModTenPower(int power, long target, HashSet<Long> valuesModPrevious) {
+
+		// easy way of avoiding repeated numbers, when they're multiples of more than one of the input
+		// values - COULD be more efficient though FIXME
+		
+		
+		HashSet<Long> output = new HashSet<>();
+					
+		long tenPower = (long) Math.pow(10, power);
+		
+		for(long prevValue : valuesModPrevious) {
+			long testValue = 0;
+			long maxValue = tenPower - 1;
+			while(testValue < maxValue) {
+				
+				if((testValue * testValue) % tenPower == target) {
+					output.add(testValue);
+					System.out.println("detuct that " + testValue + " squares to " + target);
+				}
+				
+				testValue += prevValue;
+			}
+		}
+		
+		return output;
+	}
 }
